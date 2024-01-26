6051efb413664824fcc6e7f9c6927341b967496b
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-01-26 23:44:30 +0100

    Update GatewayConfig.java

--- src/main/java/com/microservices/config/GatewayConfig.java
+++ src/main/java/com/microservices/config/GatewayConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
-
 @Configuration
 public class GatewayConfig {
 
@@ -20,13 +19,15 @@ public class GatewayConfig {
                         .method("PUT")
                         .and()
                         .path("/patients/**")
-                        .uri("http://172.17.0.2:8082"))
+                      //  .uri("http://172.17.0.2:8082"))
+                        .uri("http://localhost:8082"))
+
            
                 .route("add_patient_route", r -> r
                         .method("POST")
                         .and()
                         .path("/patients/**")
-                        .uri("http://172.17.0.2:8082"))
+                        .uri("http://localhost:8082"))
                 .build();
     }
 }
