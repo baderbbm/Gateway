c4b504a33b595554e391a15ad77cddc3d019535d
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-02-27 21:44:54 +0100

    Update AuthenticationController.java

--- src/main/java/com/microservices/controller/AuthenticationController.java
+++ src/main/java/com/microservices/controller/AuthenticationController.java
@@ -1,1 +1,32 @@
+package com.microservices.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+	
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate(@RequestParam String username, @RequestParam String password) {
+    	 System.out.println("je suis la OK");
+      
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        
+        if (authentication != null && authentication.isAuthenticated()) {
+	        System.out.println("OK");
+
+            // Authentification réussie, renvoyer une réponse de redirection vers "/afficher-patients"
+            return ResponseEntity.status(HttpStatus.FOUND).header("Location", "/afficher-patients").build();
+        } else {
+	        System.out.println("NOT OK");
+
+            // Échec de l'authentification, renvoyer une réponse avec un code d'erreur
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Échec de l'authentification");
+        }
+    }
+}
